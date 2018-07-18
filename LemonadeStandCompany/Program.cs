0a064fbdd086b74fe383a674e9f388898b961a63
Evan <ecottrellhomes@gmail.com>
2018-07-18 10:36:29 -0500

    Added Welcome statments at headquarters

---
+++ LemonadeStandCompany/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LemonadeStandCompany
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+            Console.WriteLine("Welcome to '2 Good' Lemonade Stand Headquarters");
+
+            Console.WriteLine("How many lemonade stands do you want to open?");
+        }
+    }
+}
